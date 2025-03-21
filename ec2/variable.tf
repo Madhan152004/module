83a4af105000e4ebf629008b51eccae20701d896
Madhan152004 <139326566+Madhan152004@users.noreply.github.com>
2025-03-21 19:45:56 +0530

    Create variable.tf

---
+++ ec2/variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+    default = "This is ami"
+  
+}
+
+variable "instance_type" {
+    default = "This is Instance type"
+  
+}
+
+variable "name" {
+
+    default = "This is name"
+  
+}
